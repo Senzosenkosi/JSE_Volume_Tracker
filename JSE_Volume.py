00d00f265b32081da7becb64f4a17dfbb345f548
Senzosenkosi <senzoshezi08@gmail.com>
2025-03-02 23:34:00 +0200

    first_commit

--- JSE_Volume.py
+++ JSE_Volume.py
@@ -11,38 +11,56 @@ import matplotlib.pyplot as plt
 
 def process_file(file_path):
     # Read the CSV file
+    file_path='/Users/senzosenkosishezi/Desktop/JSE_Volume_Tracker/JSE_Volume_Tracker/Time Series (GND.JSE)_2025-03-01T22_32_02+02_00.csv'
    #file_path ='/Users/senzosenkosishezi/Desktop/JSE_Volume_Tracker/JSE_Volume_Tracker/Course of Sales (GRT.JSE)_2025-02-27T17_05_47+02_00.csv'
     JSE_symbol = re.search(r'\((.*?)\)', file_path)
     Date_trade_volume = re.search(r'\d{4}-\d{2}-\d{2}', file_path)
-    symbol_data= pd.read_csv(file_path)
+    cols=['Date','Open','High',	'Low','Close','Volume','Value','MktVWAP','Transactions','Adj. Factor']
+    symbol_data= pd.read_csv(file_path,skiprows=0,usecols=cols)
+    # symbol_data=symbol_data.where(symbol_data['Date'] > '10-Feb-25')
+    #print(symbol_data['Date'].to_timestamp())
+    # symbol_data['Date'] = pd.to_datetime(symbol_data['Date'])  
+    sns.relplot(
+    data=symbol_data, x="Date", y="Value",
+    size="Date", sizes=(15, 200)
+    )
+    plt.show()
+    
+    # sns.relplot(x='Value', y='Date', data=symbol_data,hue='Date',kind='scatter')
+    # # sns.lineplot(data=symbol_data.where(symbol_data['Date'] > '10-Feb-25'), x='Date', y='Value', marker='o')
+    # # plt.title(f'Value Over Time for {JSE_symbol}')
+    # # plt.xlabel('Date')
+    # # plt.ylabel('Value')
+    # # plt.xticks(rotation=45)
+    # plt.show()
     
     # Replace empty strings with NaN
-    symbol_data.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+    # symbol_data.replace(r'^\s*$', np.nan, regex=True, inplace=True)
     
-    symbol_data.replace(r'^\s*$', np.nan, regex=True)
-    symbol_data['Traded_Volume'] = symbol_data['Value'].replace('-', 'MATCH')
-    # Replace '-' with 'MATCH' in 'Value' column
+    # symbol_data.replace(r'^\s*$', np.nan, regex=True)
+    # symbol_data['Traded_Volume'] = symbol_data['Value'].replace('-', 'MATCH')
+    # # Replace '-' with 'MATCH' in 'Value' column
     
     
-    symbol_data['Rand_Value'] = symbol_data['Volume'].str.split('  ', expand=True)
-    symbol_data['Rand_Value']=symbol_data['Rand_Value'].astype(str)
-    symbol_data['Rand_Value']=symbol_data['Rand_Value'].replace(',','',regex=True)
-    symbol_data['Rand_Value']=symbol_data['Rand_Value'].astype(float)
+    # symbol_data['Rand_Value'] = symbol_data['Volume'].str.split('  ', expand=True)
+    # symbol_data['Rand_Value']=symbol_data['Rand_Value'].astype(str)
+    # symbol_data['Rand_Value']=symbol_data['Rand_Value'].replace(',','',regex=True)
+    # symbol_data['Rand_Value']=symbol_data['Rand_Value'].astype(float)
 
-    aggregated_volume = symbol_data.groupby('Traded_Volume')['Rand_Value'].sum()
+    # aggregated_volume = symbol_data.groupby('Traded_Volume')['Rand_Value'].sum()
     
-    Total_ASK = aggregated_volume['ASK']
-    Total_BID = aggregated_volume['BID']
-    Total_MATCH = aggregated_volume['MATCH']
+    # Total_ASK = aggregated_volume['ASK']
+    # Total_BID = aggregated_volume['BID']
+    # Total_MATCH = aggregated_volume['MATCH']
     
     
-    share_data_info_per_date= aggregated_volume.to_list()
-    share_data_info_per_date.append(JSE_symbol.group(1))
-    share_data_info_per_date.append(Date_trade_volume.group())
-    share_data_info_per_date.append(Total_ASK+Total_BID+Total_MATCH)
-    # final_Data = ['BID','ASK','MATCH']
+    # share_data_info_per_date= aggregated_volume.to_list()
+    # share_data_info_per_date.append(JSE_symbol.group(1))
+    # share_data_info_per_date.append(Date_trade_volume.group())
+    # share_data_info_per_date.append(Total_ASK+Total_BID+Total_MATCH)
+    # # final_Data = ['BID','ASK','MATCH']
     
-    return share_data_info_per_date
+    # return share_data_info_per_date
    
     
     # dicti={'TOT_BID':0,'TOT_ASK':0,'MATCH':0,'SYMBOL':0,'Date':0}
@@ -73,6 +91,7 @@ def plt__line_graph(share_data_info_per_date):
 list_of_files = []    
 for x in os.listdir():
     if x.endswith(".csv"):   
+        print("file being processed is: ", x)
         processed_file = process_file(x)
         list_of_files.append(processed_file)
        
@@ -80,6 +99,18 @@ for x in os.listdir():
 All_share_data=pd.DataFrame(list_of_files,columns = ['Shares_Sold' , 'Shares_Bought', 'MATCH' , 'SYMBOL', 'Date','ToT_Volume_Traded',]) 
 all_share_data=All_share_data.sort_values(by='Date')
 
-plt__line_graph(all_share_data.where(all_share_data['SYMBOL']=='GRT.JSE'))
-print(All_share_data)
 
+
+
+
+
+    volume = row['Volume']
+    print('m' in volume)
+    if 'k' in volume:
+        symbol_data['Volume'] = symbol_data['Volume'].str.replace('k', '')
+        row['Volume'] = row['Volume'].replace(',', '').astype(float) * 1000
+        print("worked")
+    # Value = row['Value']    
+    # if 'k' in Value:
+    #     row['Value'] = row['Value'].replace('k', '')
+    #     row['Value'] = row['Value'].replace(',', '').astype(float) * 1000   
---
+++ graph.py
@@ -0,0 +1,85 @@
+import seaborn as sns
+import os
+import pandas as pd
+import numpy as np
+import re
+from datetime import datetime
+import datetime as dt
+import matplotlib.pyplot as plt
+
+
+file_path='/Users/senzosenkosishezi/Desktop/JSE_Volume_Tracker/JSE_Volume_Tracker/Time Series (GND.JSE)_2025-03-01T22_32_02+02_00.csv'
+#file_path ='/Users/senzosenkosishezi/Desktop/JSE_Volume_Tracker/JSE_Volume_Tracker/Course of Sales (GRT.JSE)_2025-02-27T17_05_47+02_00.csv'
+JSE_symbol = re.search(r'\((.*?)\)', file_path)
+Date_trade_volume = re.search(r'\d{4}-\d{2}-\d{2}', file_path)
+cols=['Date','Open','High',	'Low','Close','Volume','Value','MktVWAP','Transactions','Adj. Factor']
+symbol_data= pd.read_csv(file_path,skiprows=0,usecols=cols)
+
+symbol_data['Date'] = pd.to_datetime(symbol_data['Date'])
+symbol_data['week' ] = symbol_data['Date'].dt.to_period('W').dt.to_timestamp()
+symbol_data['year'] = symbol_data['Date'].dt.year
+
+symbol_data.set_index('Date', inplace=True)
+for index, row in symbol_data.iterrows():
+    
+    if 'k' in row['Volume']:
+        row['Volume'] = row['Volume'].replace('k', '')
+        row['Volume'] = float(row['Volume']) * 1000  
+        symbol_data.at[index, 'Volume'] = row['Volume']
+    elif 'm' in row['Volume']:
+        row['Volume'] = row['Volume'].replace('m', '')
+        row['Volume'] = float(row['Volume']) * 1000000
+        symbol_data.at[index, 'Volume'] = row['Volume']
+        
+    if 'm' in row['Value']: 
+        row['Value'] = row['Value'].replace('m', '')
+        row['Value']= float(row['Value']) * 1000000
+        symbol_data.at[index, 'Value'] = row['Value']
+    elif 'k' in row['Value']:
+        row['Value'] = row['Value'].replace('k', '')
+        row['Value'] = float(row['Value']) * 1000
+        symbol_data.at[index, 'Value'] = row['Value']
+
+    if float(row['Value']) < 1000000:
+        symbol_data.at[index, 'Value_group'] = 'Less than 1M'
+    elif float(row['Value']) < 5000000 and float(row['Value']) > 1000000:
+        symbol_data.at[index, 'Value_group']  = '1M to 5M'        
+    elif float(row['Value']) < 10000000 and float(row['Value']) > 5000000:
+        symbol_data.at[index, 'Value_group']  = '5M to 10M'
+    elif float(row['Value']) < 100000000 and float(row['Value']) > 10000000:  
+        symbol_data.at[index, 'Value_group']  = '10M to 100M'  
+    else:
+        symbol_data.at[index, 'Value_group']  = 'More than 100M'
+    
+    
+    
+plt.plot(symbol_data['Value'], label='Value', color='blue')   
+plt.title('GND.JSE Value Over Time')
+plt.xlabel('Date')
+plt.ylabel('Volume Traded')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6) # Add a grid for easier reading
+    
+plt.tight_layout() #prevents labels from being cut off
+plt.show() 
+ 
+ 
+ 
+    
+    
+# print(symbol_data['Value_group'].unique())
+# # symbol_data=symbol_data.where(symbol_data['Date'] > '10-Feb-25')
+# #print(symbol_data['Date'].to_timestamp())
+# # symbol_data['Date'] = pd.to_datetime(symbol_data['Date'])  
+# filter=symbol_data['year'] > 2023
+# filter1=symbol_data['week'] > '2024-12-24'
+# sns.scatterplot(data=symbol_data.where(filter1), x="Date", y="Value", hue="Value_group")
+
+# # sns.barplot(symbol_data, x="Value_group", y="week",errorbar=None)
+# #sns.lineplot(symbol_data.where(filter1), x="Date", y="Value", hue="Value_group")
+# # sns.relplot(
+# # data=symbol_data.where(filter), x="week", y="Value_group",
+# # size="Value_group", sizes=(15, 200)
+# # )
+# plt.show()
+
